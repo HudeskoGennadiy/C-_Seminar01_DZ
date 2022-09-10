4cdc13e7736d33d5dce43e0906d69f5548d57135
Gennadiy Khudesko <gennadiy.18224@gmail.com>
2022-09-10 19:26:18 +0300

    add new folder Task_01

---
+++ Desktop/C#_Seminar_01_DZ/Task_01/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите целое число a: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите целое число b: ");
+
+int b = Convert.ToInt32(Console.ReadLine());
+if(a > b)
+{
+    Console.WriteLine("max: " + a);
+
+    Console.WriteLine("min: " + b);
+} 
+else 
+{
+    Console.WriteLine("max: " + b);
+
+    Console.WriteLine("min: " + a);
+
+}
